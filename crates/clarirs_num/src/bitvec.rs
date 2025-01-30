6e3f29c008fc4e4e82e1711c282e54cd2ff24937
spshah1701 <sshah210@asu.edu>
2025-01-30 10:45:03 -0700

    cargo fmt

--- crates/clarirs_num/src/bitvec.rs
+++ crates/clarirs_num/src/bitvec.rs
@@ -220,32 +220,32 @@ impl BitVec {
     pub fn rotate_left(&self, rotate_amount: usize) -> Result<Self, BitVecError> {
         use num_bigint::BigUint;
         use num_traits::One;
-    
+
         let rotate = rotate_amount % self.len();
         let bit_length = self.len();
         let value_biguint = self.to_biguint();
         let mask = (BigUint::one() << bit_length) - BigUint::one();
-    
+
         let left_shifted = (&value_biguint << rotate) & &mask;
         let right_shifted = &value_biguint >> (bit_length - rotate);
         let rotated_biguint = left_shifted | right_shifted;
-    
+
         BitVec::from_biguint(&rotated_biguint, bit_length)
     }
 
     pub fn rotate_right(&self, rotate_amount: usize) -> Result<Self, BitVecError> {
         use num_bigint::BigUint;
         use num_traits::One;
-    
+
         let rotate = rotate_amount % self.len();
         let bit_length = self.len();
         let value_biguint = self.to_biguint();
         let mask = (BigUint::one() << bit_length) - BigUint::one();
-    
+
         let right_shifted = &value_biguint >> rotate;
         let left_shifted = (&value_biguint << (bit_length - rotate)) & &mask;
         let rotated_biguint = (right_shifted | left_shifted) & &mask;
-    
+
         BitVec::from_biguint(&rotated_biguint, bit_length)
     }
 
