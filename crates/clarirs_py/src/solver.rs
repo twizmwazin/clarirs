24e9889f835d95e41c7d278e5c5d00802a77d55d
Kevin Phoenix <kevin@kphoenix.us>
2025-02-11 10:00:35 -0700

    Add python z3 solver bindings (#89)

--- crates/clarirs_py/src/solver.rs
+++ crates/clarirs_py/src/solver.rs
@@ -1,4 +1,5 @@
 use crate::prelude::*;
+use clarirs_z3::Z3Solver;
 
 #[pyclass(name = "ConcreteSolver", module = "clarirs.solver")]
 pub struct PyConcreteSolver {
@@ -82,7 +83,91 @@ impl PyConcreteSolver {
     }
 }
 
+#[pyclass(name = "Z3Solver", module = "clarirs.solver")]
+pub struct PyZ3Solver {
+    inner: Z3Solver<'static>,
+}
+
+#[pymethods]
+impl PyZ3Solver {
+    #[new]
+    fn new() -> Result<PyClassInitializer<Self>, ClaripyError> {
+        Ok(PyClassInitializer::from(Self {
+            inner: Z3Solver::new(&GLOBAL_CONTEXT),
+        }))
+    }
+
+    fn add(&mut self, expr: Bound<Bool>) -> Result<(), ClaripyError> {
+        Ok(self.inner.add(&expr.get().inner)?)
+    }
+
+    fn eval(&mut self, py: Python, expr: Bound<Base>) -> Result<Py<Base>, ClaripyError> {
+        if let Ok(bv_value) = expr.clone().into_any().downcast::<BV>() {
+            BV::new(py, &self.inner.eval_bitvec(&bv_value.get().inner)?).map(|b| {
+                b.into_any()
+                    .downcast_bound::<Base>(py)
+                    .unwrap()
+                    .clone()
+                    .unbind()
+            })
+        } else if let Ok(bool_value) = expr.clone().into_any().downcast::<Bool>() {
+            Bool::new(py, &self.inner.eval_bool(&bool_value.get().inner).unwrap()).map(|b| {
+                b.into_any()
+                    .downcast_bound::<Base>(py)
+                    .unwrap()
+                    .clone()
+                    .unbind()
+            })
+        } else if let Ok(fp_value) = expr.clone().into_any().downcast::<FP>() {
+            FP::new(py, &self.inner.eval_float(&fp_value.get().inner)?).map(|b| {
+                b.into_any()
+                    .downcast_bound::<Base>(py)
+                    .unwrap()
+                    .clone()
+                    .unbind()
+            })
+        } else if let Ok(string_value) = expr.clone().into_any().downcast::<PyAstString>() {
+            PyAstString::new(py, &self.inner.eval_string(&string_value.get().inner)?).map(|b| {
+                b.into_any()
+                    .downcast_bound::<Base>(py)
+                    .unwrap()
+                    .clone()
+                    .unbind()
+            })
+        } else {
+            panic!("Unsupported type");
+        }
+    }
+
+    fn batch_eval(
+        &mut self,
+        py: Python,
+        exprs: Vec<Bound<Base>>,
+        _max_solutions: u32, // Z3 solver does not support multiple solutions
+    ) -> Result<Vec<Py<Base>>, ClaripyError> {
+        exprs.into_iter().map(|expr| self.eval(py, expr)).collect()
+    }
+
+    fn is_true(&mut self, expr: Bound<Bool>) -> Result<bool, ClaripyError> {
+        Ok(self.inner.is_true(&expr.get().inner).unwrap())
+    }
+
+    fn is_false(&mut self, expr: Bound<Bool>) -> Result<bool, ClaripyError> {
+        Ok(self.inner.is_false(&expr.get().inner).unwrap())
+    }
+
+    fn min(&mut self, py: Python, expr: Bound<BV>) -> Result<Py<BV>, ClaripyError> {
+        BV::new(py, &self.inner.min(&expr.get().inner).unwrap())
+    }
+
+    fn max(&mut self, py: Python, expr: Bound<BV>) -> Result<Py<BV>, ClaripyError> {
+        BV::new(py, &self.inner.max(&expr.get().inner).unwrap())
+    }
+}
+
 pub(crate) fn import(_: Python, m: &Bound<PyModule>) -> PyResult<()> {
     m.add_class::<PyConcreteSolver>()?;
+    m.add_class::<PyZ3Solver>()?;
+
     Ok(())
 }
