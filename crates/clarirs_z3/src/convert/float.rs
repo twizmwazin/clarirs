94dc6d1e7ba8e238b9fabd594848471807797a3c
Kevin Phoenix <kevin@kphoenix.us>
2025-02-20 00:25:34 -0700

    Cargo fmt

--- crates/clarirs_z3/src/convert/float.rs
+++ crates/clarirs_z3/src/convert/float.rs
@@ -1,14 +1,10 @@
 use clarirs_core::prelude::*;
 use clarirs_z3_sys as z3;
 
-pub fn convert_float_to_z3(
-    _ast: &FloatAst,
-) -> Result<z3::Ast, ClarirsError> {
+pub fn convert_float_to_z3(_ast: &FloatAst) -> Result<z3::Ast, ClarirsError> {
     todo!()
 }
 
-pub fn convert_float_from_z3<'c>(
-    _ast: z3::Ast,
-) -> Result<FloatAst<'c>, ClarirsError> {
+pub fn convert_float_from_z3<'c>(_ast: z3::Ast) -> Result<FloatAst<'c>, ClarirsError> {
     todo!()
 }
--- crates/clarirs_z3/src/convert/string.rs
+++ crates/clarirs_z3/src/convert/string.rs
@@ -1,14 +1,10 @@
 use clarirs_core::prelude::*;
 use clarirs_z3_sys as z3;
 
-pub fn convert_string_to_z3(
-    _ast: &StringAst,
-) -> Result<z3::Ast, ClarirsError> {
+pub fn convert_string_to_z3(_ast: &StringAst) -> Result<z3::Ast, ClarirsError> {
     todo!()
 }
 
-pub fn convert_string_from_z3<'c>(
-    _ast: z3::Ast,
-) -> Result<StringAst<'c>, ClarirsError> {
+pub fn convert_string_from_z3<'c>(_ast: z3::Ast) -> Result<StringAst<'c>, ClarirsError> {
     todo!()
 }
