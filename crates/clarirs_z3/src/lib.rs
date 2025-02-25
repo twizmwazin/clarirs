0a3075d321226cb1771e04fc3e7387a3770fb1bc
Kevin Phoenix <kevin@kphoenix.us>
2025-02-25 14:51:24 -0700

    Add z3 error handler to panic on z3 errors (#128)

--- crates/clarirs_z3/src/lib.rs
+++ crates/clarirs_z3/src/lib.rs
@@ -4,10 +4,16 @@ mod solver;
 
 use clarirs_z3_sys as z3;
 
+/// Error handler that panics on Z3 errors
+unsafe extern "C" fn panic_on_error(_: z3::Context, e: z3::ErrorCode) {
+    panic!("Z3 error: {:?}", e);
+}
+
 thread_local! {
     static Z3_CONTEXT: z3::Context = unsafe {
         let cfg = z3::mk_config();
         let ctx = z3::mk_context_rc(cfg);
+        z3::set_error_handler(ctx, Some(panic_on_error));
         z3::del_config(cfg);
         ctx
     }
