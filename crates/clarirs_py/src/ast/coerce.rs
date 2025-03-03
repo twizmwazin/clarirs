7265cc1224c3a346565ccb76cc5d75333ec735f1
Kevin Phoenix <kevin@kphoenix.us>
2025-03-02 22:48:19 -0700

    Refactor CoerceBV into a more transparent enum (#153)
    * Refactor CoerceBV into a more transparent enum
    
    * Cargo fmt

--- crates/clarirs_py/src/ast/coerce.rs
+++ crates/clarirs_py/src/ast/coerce.rs
@@ -1,6 +1,7 @@
-use std::str;
+use std::{cmp::max, str};
 
-use clarirs_core::ast::bitvec::BitVecExt;
+use num_bigint::BigInt;
+use pyo3::types::PyInt;
 
 use crate::prelude::*;
 
@@ -32,47 +33,30 @@ impl<'py> From<CoerceBool<'py>> for BoolAst<'static> {
     }
 }
 
-pub struct CoerceBV<'py> {
-    inner: Bound<'py, BV>,
-    coerced: bool,
+pub enum CoerceBV<'py> {
+    BV(Bound<'py, BV>),
+    Int(BigInt),
 }
 
 impl<'py> CoerceBV<'py> {
-    pub fn new(bv: Bound<'py, BV>) -> Self {
-        CoerceBV {
-            inner: bv,
-            coerced: false,
-        }
-    }
-
-    pub fn new_coerced(bv: Bound<'py, BV>) -> Self {
-        CoerceBV {
-            inner: bv,
-            coerced: true,
+    pub fn extract(&self, py: Python<'py>, size: u32) -> Result<Bound<'py, BV>, ClaripyError> {
+        match self {
+            CoerceBV::BV(bv) => {
+                if bv.get().size() as u32 == size {
+                    Ok(bv.clone())
+                } else {
+                    Err(ClaripyError::CastingError("BV size mismatch".to_string()))
+                }
+            }
+            CoerceBV::Int(int) => {
+                let bv = BitVec::from_bigint_trunc(int, size);
+                BV::new(py, &GLOBAL_CONTEXT.bvv(bv)?)
+            }
         }
     }
 
     pub fn extract_like(&self, py: Python<'py>, like: &BV) -> Result<Bound<'py, BV>, ClaripyError> {
-        let our_size = self.inner.get().inner.size();
-        let like_size = like.inner.size();
-
-        if self.coerced && like_size != our_size {
-            match self.inner.get().inner.op() {
-                BitVecOp::BVV(val) => BV::new(
-                    py,
-                    &GLOBAL_CONTEXT.bvv_from_biguint_with_size(&val.to_biguint(), like_size)?,
-                ),
-                _ => {
-                    if like_size > our_size {
-                        self.inner.get().zero_extend(py, like_size)
-                    } else {
-                        self.inner.get().Extract(py, like_size - 1, 0)
-                    }
-                }
-            }
-        } else {
-            Ok(self.inner.clone())
-        }
+        self.extract(py, like.size() as u32)
     }
 
     pub fn extract_pair(
@@ -80,10 +64,31 @@ impl<'py> CoerceBV<'py> {
         lhs: &CoerceBV<'py>,
         rhs: &CoerceBV<'py>,
     ) -> Result<(Bound<'py, BV>, Bound<'py, BV>), ClaripyError> {
-        Ok(match (lhs.coerced, rhs.coerced) {
-            (true, true) | (false, false) => (lhs.inner.clone(), rhs.inner.clone()),
-            (true, false) => (lhs.extract_like(py, rhs.inner.get())?, rhs.inner.clone()),
-            (false, true) => (lhs.inner.clone(), rhs.extract_like(py, lhs.inner.get())?),
+        Ok(match (lhs, rhs) {
+            (CoerceBV::BV(lhs), CoerceBV::BV(rhs)) => (lhs.clone(), rhs.clone()),
+            (CoerceBV::Int(_), CoerceBV::BV(rhs)) => {
+                let lhs = lhs.extract_like(py, rhs.get())?;
+                (lhs, rhs.clone())
+            }
+            (CoerceBV::BV(lhs), CoerceBV::Int(_)) => {
+                let rhs = rhs.extract_like(py, lhs.get())?;
+                (lhs.clone(), rhs)
+            }
+            (CoerceBV::Int(lhs_int), CoerceBV::Int(rhs_int)) => {
+                // Both args are ints, so we kinda have to guess the size
+                // Take the max size of the two ints, then round up to the nearest power of 2
+
+                let mut size = max(lhs_int.bits() as u32, rhs_int.bits() as u32);
+                if *lhs_int < BigInt::ZERO || *rhs_int < BigInt::ZERO {
+                    // If either int is negative, we need to add an extra bit for the sign
+                    size += 1;
+                }
+                let size = size.next_power_of_two();
+
+                let lhs = lhs.extract(py, size)?;
+                let rhs = rhs.extract(py, size)?;
+                (lhs, rhs)
+            }
         })
     }
 }
@@ -91,32 +96,24 @@ impl<'py> CoerceBV<'py> {
 impl<'py> FromPyObject<'py> for CoerceBV<'py> {
     fn extract_bound(val: &Bound<'py, PyAny>) -> PyResult<Self> {
         if let Ok(bv_val) = val.downcast::<BV>() {
-            Ok(CoerceBV::new(bv_val.clone()))
-        } else if let Ok(bv_val) = val.extract::<u64>() {
-            Ok(CoerceBV::new_coerced(
-                BV::new(val.py(), &GLOBAL_CONTEXT.bvv_prim(bv_val).unwrap()).unwrap(),
-            ))
+            Ok(CoerceBV::from(bv_val.clone()))
+        } else if let Ok(int_val) = val.downcast::<PyInt>() {
+            Ok(CoerceBV::from(int_val.clone()))
         } else {
             Err(ClaripyError::InvalidArgumentType("Expected BV".to_string()).into())
         }
     }
 }
 
-impl<'py> From<CoerceBV<'py>> for Bound<'py, BV> {
-    fn from(val: CoerceBV<'py>) -> Self {
-        val.inner
-    }
-}
-
 impl<'py> From<Bound<'py, BV>> for CoerceBV<'py> {
     fn from(val: Bound<'py, BV>) -> Self {
-        CoerceBV::new(val)
+        CoerceBV::BV(val)
     }
 }
 
-impl<'py> From<CoerceBV<'py>> for BitVecAst<'static> {
-    fn from(val: CoerceBV<'py>) -> Self {
-        val.inner.get().inner.clone()
+impl<'py> From<Bound<'py, PyInt>> for CoerceBV<'py> {
+    fn from(val: Bound<'py, PyInt>) -> Self {
+        CoerceBV::Int(val.extract::<BigInt>().unwrap())
     }
 }
 
