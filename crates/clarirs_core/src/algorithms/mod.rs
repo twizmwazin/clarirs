68750aea96c4733a100ad1b6c2432bd757aaf94b
Kevin Phoenix <kevin@kphoenix.us>
2025-06-19 15:22:30 -0700

    Cargo fmt

--- crates/clarirs_core/src/algorithms/mod.rs
+++ crates/clarirs_core/src/algorithms/mod.rs
@@ -7,9 +7,9 @@ pub mod post_order;
 pub mod replace;
 pub mod simplify;
 
+pub use deannotate::Deannotate;
 pub use excavate_ite::ExcavateIte;
 pub use join::Join;
-pub use deannotate::Deannotate;
 pub use post_order::walk_post_order;
 pub use replace::Replace;
 pub use simplify::Simplify;
