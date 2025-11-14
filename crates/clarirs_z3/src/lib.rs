a71fd7c27c1dc8da27312ad6b69d120761c1524f
Kevin Phoenix <kevin@kphoenix.us>
2025-11-14 15:37:24 -0700

    Improve z3 error message handling

--- crates/clarirs_z3/src/lib.rs
+++ crates/clarirs_z3/src/lib.rs
@@ -2,24 +2,24 @@ mod astext;
 mod rc;
 mod solver;
 
-use std::ffi::CStr;
+pub use solver::Z3Solver;
 
 use clarirs_z3_sys as z3;
-
-/// Error handler that panics on Z3 errors
-unsafe extern "C" fn panic_on_error(ctx: z3::Context, e: z3::ErrorCode) {
-    let msg = unsafe { CStr::from_ptr(z3::get_error_msg(ctx, e)) };
-    panic!("Z3 error: {msg:?}");
-}
 
 thread_local! {
     static Z3_CONTEXT: z3::Context = unsafe {
         let cfg = z3::mk_config();
-        let ctx = z3::mk_context_rc(cfg);
-        z3::set_error_handler(ctx, Some(panic_on_error));
+        let ctx = z3::mk_context(cfg);
         z3::del_config(cfg);
         ctx
     }
 }
 
-pub use solver::Z3Solver;
+pub(crate) fn get_z3_error_msg() -> String {
+    Z3_CONTEXT.with(|z3_ctx| unsafe {
+        let error_code = z3::get_error_code(*z3_ctx);
+        let err_msg = z3::get_error_msg(*z3_ctx, error_code);
+        let c_str = std::ffi::CStr::from_ptr(err_msg);
+        c_str.to_string_lossy().into_owned()
+    })
+}
