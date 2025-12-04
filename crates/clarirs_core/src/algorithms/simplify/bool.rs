b1571b41baa5f0244bcb138d91367bc7cfb0f4aa
Kevin Phoenix <kevin@kphoenix.us>
2025-12-04 11:01:16 -0700

    Fix some boolean ops retunring unsimplified children

--- crates/clarirs_core/src/algorithms/simplify/bool.rs
+++ crates/clarirs_core/src/algorithms/simplify/bool.rs
@@ -24,10 +24,8 @@ pub(crate) fn simplify_bool<'c>(
 
             match (early_lhs.op(), early_rhs.op()) {
                 (BooleanOp::BoolV(lhs), BooleanOp::BoolV(rhs)) => Ok(ctx.boolv(*lhs && *rhs)?),
-
-                (BooleanOp::BoolV(true), v) | (v, BooleanOp::BoolV(true)) => {
-                    Ok(ctx.make_bool(v.clone())?)
-                }
+                (BooleanOp::BoolV(true), _) => Ok(state.get_bool_simplified(1)?),
+                (_, BooleanOp::BoolV(true)) => Ok(state.get_bool_simplified(0)?),
                 (BooleanOp::BoolV(false), _) | (_, BooleanOp::BoolV(false)) => Ok(ctx.false_()?),
                 (BooleanOp::Not(lhs), rhs) if lhs.op() == rhs => Ok(ctx.false_()?),
                 (lhs, BooleanOp::Not(rhs)) if lhs == rhs.op() => Ok(ctx.false_()?),
@@ -43,9 +41,8 @@ pub(crate) fn simplify_bool<'c>(
             match (early_lhs.op(), early_rhs.op()) {
                 (BooleanOp::BoolV(lhs), BooleanOp::BoolV(rhs)) => Ok(ctx.boolv(*lhs || *rhs)?),
                 (BooleanOp::BoolV(true), _) | (_, BooleanOp::BoolV(true)) => Ok(ctx.true_()?),
-                (BooleanOp::BoolV(false), v) | (v, BooleanOp::BoolV(false)) => {
-                    Ok(ctx.make_bool(v.clone())?)
-                }
+                (BooleanOp::BoolV(false), _) => Ok(state.get_bool_simplified(1)?),
+                (_, BooleanOp::BoolV(false)) => Ok(state.get_bool_simplified(0)?),
                 (BooleanOp::Not(lhs), rhs) if lhs.op() == rhs => Ok(ctx.true_()?),
                 (lhs, BooleanOp::Not(rhs)) if lhs == rhs.op() => Ok(ctx.true_()?),
                 (BooleanOp::Not(lhs), BooleanOp::Not(rhs)) => Ok(ctx.not(ctx.and(lhs, rhs)?)?),
@@ -59,12 +56,10 @@ pub(crate) fn simplify_bool<'c>(
 
             match (early_lhs.op(), early_rhs.op()) {
                 (BooleanOp::BoolV(lhs), BooleanOp::BoolV(rhs)) => Ok(ctx.boolv(*lhs ^ *rhs)?),
-                (BooleanOp::BoolV(true), v) | (v, BooleanOp::BoolV(true)) => {
-                    Ok(ctx.not(ctx.make_bool(v.clone())?)?)
-                }
-                (BooleanOp::BoolV(false), v) | (v, BooleanOp::BoolV(false)) => {
-                    Ok(ctx.make_bool(v.clone())?)
-                }
+                (BooleanOp::BoolV(true), _) => Ok(ctx.not(state.get_bool_simplified(1)?)?),
+                (_, BooleanOp::BoolV(true)) => Ok(ctx.not(state.get_bool_simplified(0)?)?),
+                (BooleanOp::BoolV(false), _) => Ok(state.get_bool_simplified(1)?),
+                (_, BooleanOp::BoolV(false)) => Ok(state.get_bool_simplified(0)?),
                 (BooleanOp::Not(lhs), rhs) if lhs.op() == rhs => Ok(ctx.true_()?),
                 (lhs, BooleanOp::Not(rhs)) if lhs == rhs.op() => Ok(ctx.true_()?),
                 (BooleanOp::Not(lhs), BooleanOp::Not(rhs)) => state.rerun(ctx.xor(lhs, rhs)?),
@@ -78,12 +73,10 @@ pub(crate) fn simplify_bool<'c>(
 
             match (early_lhs.op(), early_rhs.op()) {
                 (BooleanOp::BoolV(arc), BooleanOp::BoolV(arc1)) => Ok(ctx.boolv(arc == arc1)?),
-                (BooleanOp::BoolV(true), v) | (v, BooleanOp::BoolV(true)) => {
-                    Ok(ctx.make_bool(v.clone())?)
-                }
-                (BooleanOp::BoolV(false), v) | (v, BooleanOp::BoolV(false)) => {
-                    Ok(ctx.not(ctx.make_bool(v.clone())?)?)
-                }
+                (BooleanOp::BoolV(true), _) => Ok(state.get_bool_simplified(1)?),
+                (_, BooleanOp::BoolV(true)) => Ok(state.get_bool_simplified(0)?),
+                (BooleanOp::BoolV(false), _) => Ok(ctx.not(state.get_bool_simplified(1)?)?),
+                (_, BooleanOp::BoolV(false)) => Ok(ctx.not(state.get_bool_simplified(0)?)?),
                 _ if early_lhs == early_rhs => Ok(ctx.true_()?),
                 _ => Ok(ctx.eq_(state.get_bool_simplified(0)?, state.get_bool_simplified(1)?)?),
             }
@@ -94,12 +87,10 @@ pub(crate) fn simplify_bool<'c>(
 
             match (early_lhs.op(), early_rhs.op()) {
                 (BooleanOp::BoolV(arc), BooleanOp::BoolV(arc1)) => Ok(ctx.boolv(arc != arc1)?),
-                (BooleanOp::BoolV(true), v) | (v, BooleanOp::BoolV(true)) => {
-                    Ok(ctx.not(ctx.make_bool(v.clone())?)?)
-                }
-                (BooleanOp::BoolV(false), v) | (v, BooleanOp::BoolV(false)) => {
-                    Ok(ctx.make_bool(v.clone())?)
-                }
+                (BooleanOp::BoolV(true), _) => Ok(ctx.not(state.get_bool_simplified(1)?)?),
+                (_, BooleanOp::BoolV(true)) => Ok(ctx.not(state.get_bool_simplified(0)?)?),
+                (BooleanOp::BoolV(false), _) => Ok(state.get_bool_simplified(1)?),
+                (_, BooleanOp::BoolV(false)) => Ok(state.get_bool_simplified(0)?),
                 _ if early_lhs == early_rhs => Ok(ctx.false_()?),
                 _ => Ok(ctx.neq(state.get_bool_simplified(0)?, state.get_bool_simplified(1)?)?),
             }
