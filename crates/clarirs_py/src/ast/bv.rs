5d3266539cd8523bfdd7d136c6cc57a0bf65e2ce
Kevin Phoenix <kevin@kphoenix.us>
2025-12-08 14:00:38 -0700

    Add replace to python bindings (#406)

--- crates/clarirs_py/src/ast/bv.rs
+++ crates/clarirs_py/src/ast/bv.rs
@@ -312,6 +312,19 @@ impl BV {
         BV::new(py, &self.inner.simplify_ext(respect_annotations)?)
     }
 
+    pub fn replace<'py>(
+        &self,
+        py: Python<'py>,
+        from: Bound<'py, Base>,
+        to: Bound<'py, Base>,
+    ) -> Result<Bound<'py, BV>, ClaripyError> {
+        use clarirs_core::algorithms::Replace;
+        let from_ast = Base::to_dynast(from)?;
+        let to_ast = Base::to_dynast(to)?;
+        let replaced = self.inner.replace(&from_ast, &to_ast)?;
+        BV::new(py, &replaced)
+    }
+
     pub fn size(&self) -> usize {
         self.inner.size() as usize
     }
